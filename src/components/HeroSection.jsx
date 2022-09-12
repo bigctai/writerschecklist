a06f3f2ef869489b6a61e553ceee2b358f15f498
Chris Tai <christai100154@gmail.com>
2022-09-11 23:43:49 -0400

    updated look of hero section

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -6,23 +6,30 @@ import Title from "./Title/Title";
 function HeroSection() {
   return (
     <div className="hero-container">
-      <Title titleName="Welcome to Writer's Checklist" />
-      <p>
-        Keep Track of All of Your Submissions <br></br> So You Never Miss
-        Another Deadline Again
-      </p>
-      <div className="hero-btn">
-        <Button className="btn" destination="Signup" buttonSize="btn--large">
-          Get Started
-        </Button>
-        <Button
-          className="btn"
-          destination="/ExploreSearch"
-          buttonStyle="btn--white"
-          buttonSize="btn--large"
-        >
-          Explore Literary Journals
-        </Button>
+      <img
+        className="background-image"
+        src="images/writerschecklistbackground.jpeg"
+      ></img>
+      <div className="lesser-container">
+        <div className="title">
+          <Title color="white" titleName="Welcome to" />
+          <Title color="orange" titleName="Writer's Checklist" />
+        </div>
+        <p className="first-line">Keep Track of All of Your Submissions</p>
+        <p className="important-line">So You Never Miss Another Deadline</p>
+        <div className="hero-btn">
+          <Button className="btn" destination="Signup" buttonSize="btn--large">
+            Get Started
+          </Button>
+          <Button
+            className="btn"
+            destination="/ExploreSearch"
+            buttonStyle="btn--white"
+            buttonSize="btn--large"
+          >
+            Explore Literary Journals
+          </Button>
+        </div>
       </div>
     </div>
   );
